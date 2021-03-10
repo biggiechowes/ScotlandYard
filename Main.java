bc0351210733508bc9eaef288960be29f06f86cd
Paul Vladislav <we20949@bristol.ac.uk>
2021-03-10 17:11:11 +0200

    Removed all previous files

--- Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here // change
-    System.out.print("Connor has a huge sword");
-    }
-}
