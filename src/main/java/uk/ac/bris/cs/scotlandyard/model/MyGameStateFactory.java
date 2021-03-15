40f1a82d25e69918ca14d78e0c51d9a9b5873fe0
CONNOR <oi20681@bristol.ac.uk>
2021-03-15 17:24:55 +0000

    MyGameStateFactory.java initally working

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -1,25 +1,25 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
-
+import com.google.common.collect.ImmutableSet;
+import java.util.*;
 import javax.annotation.Nonnull;
-
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
-import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+import uk.ac.bris.cs.scotlandyard.model.Move.*;
+import uk.ac.bris.cs.scotlandyard.model.Piece.*;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
 
-/**
- * cw-model
- * Stage 1: Complete this class
- */
 public final class MyGameStateFactory implements Factory<GameState> {
-
-	@Nonnull @Override public GameState build(
-			GameSetup setup,
-			Player mrX,
-			ImmutableList<Player> detectives) {
-		// TODO
-		throw new RuntimeException("Implement me!");
-
+	@Override public GameState build(GameSetup setup, Player mrX, ImmutableList<Player> detectives) { return null ;}
+	private final class MyGameState implements GameState {
+		@Override public GameSetup getSetup() {  return null; }
+		@Override public ImmutableSet<Piece> getPlayers() { return null; }
+		@Override public GameState advance(Move move) {  return null;  }
+		@Override public Optional<Integer> getDetectiveLocation(Detective detective) { return null; }
+		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) { return null; }
+		@Override public ImmutableList<LogEntry> getMrXTravelLog() { return null ;}
+		@Override public ImmutableSet<Piece> getWinner() { return null; }
+		@Override public ImmutableSet<Move> getAvailableMoves() { return null; }
+		//@Override public GameState advance(Move move) { return null ;}
 	}
-
 }
