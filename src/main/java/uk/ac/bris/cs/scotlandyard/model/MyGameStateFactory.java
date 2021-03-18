b606e9149253be484df45146d097c9233f4a6869
CONNOR <oi20681@bristol.ac.uk>
2021-03-18 16:12:23 +0000

    Passed GameStateCreationTest tests

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -21,9 +21,9 @@ public final class MyGameStateFactory implements Factory<GameState> {
 		private ImmutableList<LogEntry> log;
 		private Player mrX;
 		private List<Player> detectives;
-		private ImmutableList<Player> everyone;
+		private ImmutableSet<Piece> everyone;
 		private ImmutableSet<Move> moves;
-		private ImmutableSet<Piece> winner;
+		private ImmutableSet<Piece> winner = ImmutableSet.of();
 
 		//Constructor
 		private MyGameState(final GameSetup setup, final ImmutableSet<Piece> remaining, final ImmutableList<
@@ -33,8 +33,30 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			if (setup.rounds.isEmpty()) throw new IllegalArgumentException("Rounds is empty!");
 			if (remaining.isEmpty()) throw new IllegalArgumentException("Remaining is empty!");
 			if (mrX.isDetective()) throw new IllegalArgumentException("mrX is empty!");
+			if (mrX == null) throw new IllegalArgumentException("mrX is null");
 			if (detectives.isEmpty()) throw new IllegalArgumentException("detectives is empty!");
+			if (setup.graph.nodes().isEmpty()) throw new IllegalArgumentException("empty graph!");
 
+			List<Player> encounteredDetectives = new ArrayList<>();
+			List<Integer> encounteredLocations = new ArrayList<>();
+			for (Player p : detectives) {
+				if (p.isMrX()) throw new IllegalArgumentException("detective is mrX!");
+				if (encounteredDetectives.contains(p)) throw new IllegalArgumentException("duplicate detectives!");
+				if (encounteredLocations.contains(p.location())) throw new IllegalArgumentException("detective location overlap!");
+
+				if (p.tickets().get(Ticket.SECRET) > 0) throw new IllegalArgumentException("detective has secret ticket!");
+				if (p.tickets().get(Ticket.DOUBLE) > 0) throw new IllegalArgumentException("detective has double ticket!");
+
+
+				encounteredDetectives.add(p);
+				encounteredLocations.add(p.location());
+			}
+
+			List<Piece> pieceList = new ArrayList<>();
+			for (Player p : detectives) {
+				pieceList.add(p.piece());
+			}
+			pieceList.add(mrX.piece());
 
 
 
@@ -44,10 +66,8 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			this.log = log;
 			this.mrX = mrX;
 			this.detectives = detectives;
-			detectives.add(mrX);
-			this.everyone = ImmutableList.copyOf(detectives);
-
-			System.out.println(this.detectives.isEmpty());
+			//detectives.add(mrX);
+			this.everyone = ImmutableSet.copyOf(pieceList);
 
 		}
 		// Methods
@@ -58,36 +78,53 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			return setup;
 		}
 
-		@Override public ImmutableSet<Piece> getPlayers() {
-			return null;
+		@Nonnull @Override public ImmutableSet<Piece> getPlayers() {
+			return this.everyone;
 		}
 
 		@Nonnull @Override public Optional<Integer> getDetectiveLocation(Detective detective) {
-			for (Player p : detectives) {
+			for (Player p : this.detectives) {
 
-				System.out.println(p.piece().webColour());
-
-				if (p.piece() == detective) {
+				if (p.piece().webColour().equals(detective.webColour())) {
 					return Optional.of(p.location());
 				}
 			}
 			return Optional.empty();
 		}
-		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) {
-			/*
-			for (Player p : everyone) {
-				if (p.piece() == piece) {
-					return Optional.of();
+		@Nonnull @Override public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+
+
+			class MyTicketBoard implements TicketBoard {
+
+				final Player p;
+
+				MyTicketBoard(Player p) {
+					this.p = p;
+				}
+
+				public int getCount(@Nonnull Ticket ticket) {
+					return this.p.tickets().get(ticket);
 				}
 			}
-			*/
-			return null;
-			//test
+
+			for (Player p : this.detectives) {
+				if (p.piece() == piece) {
+					return Optional.of(new MyTicketBoard(p));
+				}
+			}
+			if (piece == this.mrX.piece()) {
+				return Optional.of(new MyTicketBoard(this.mrX));
+			}
+			return Optional.empty();
 		}
+
+
 		@Nonnull @Override public ImmutableList<LogEntry> getMrXTravelLog() {
 			return log;
 		}
-		@Override public ImmutableSet<Piece> getWinner() { return null; }
+		@Override public ImmutableSet<Piece> getWinner() {
+			return this.winner;
+		}
 		@Override public ImmutableSet<Move> getAvailableMoves() { return null; }
 
 
